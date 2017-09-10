5870a5e47a68fec7fd75bc86f69ea710dba4c08f
Frederick Ofori <FrederickCodes@gmail.com>
2017-09-10 18:51:31 +0100

    Table added to Index HTML

--- src/bowling.js
+++ src/bowling.js
@@ -5,26 +5,27 @@ function BowlingGame() {
   this.frames = []
 }
 
-
 BowlingGame.prototype.roll = function (pins){
+  var message = "You can not hit more than 10 pins in one roll"
+  if (pins > 10) throw new Error(message);
   this.frames.push(pins);
 };
-
 
 BowlingGame.prototype.calculateScore = function (){
   var totalScore = 0;
   var rollIndex = 0;
 
+  var game = this;
+
   for (var frameIndex = 0; frameIndex < 10; frameIndex++) {
-    if (this.frames[rollIndex] === 10) {
-      totalScore += this.frames[rollIndex] + this.frames[rollIndex + 1] + this.frames[rollIndex + 2];
+    if (isStrike()) {
+      totalScore += strikeScore();
       rollIndex += 1;
-    } else if (this.frames[rollIndex] + this.frames[rollIndex+1] === 10) {
-      // totalScore += this.frames[rollIndex] + this.frames[rollIndex + 1] + this.frames[rollIndex + 2];
-      totalScore += (10 + this.frames[rollIndex + 2]) ;
+    } else if (isSpare()) {
+      totalScore += spareScore();
       rollIndex += 2;
     } else {
-    totalScore += this.frames[rollIndex] + this.frames[rollIndex+1];
+    totalScore += normalScore();
     rollIndex += 2;
   }
 
@@ -33,6 +34,27 @@ BowlingGame.prototype.calculateScore = function (){
   return totalScore
 
 
+//refactoring
+
+  function isStrike() {
+    return game.frames[rollIndex] === 10;
+  }
+
+  function isSpare() {
+    return game.frames[rollIndex] + game.frames[rollIndex+1] === 10;
+  }
+
+  function spareScore() {
+    return game.frames[rollIndex] + game.frames[rollIndex + 1] + game.frames[rollIndex + 2];
+  }
+
+  function strikeScore() {
+    return game.frames[rollIndex] + game.frames[rollIndex + 1] + game.frames[rollIndex + 2];
+  }
+
+  function normalScore() {
+    return game.frames[rollIndex] + game.frames[rollIndex+1];
+  }
 
 }
 
@@ -46,7 +68,7 @@ BowlingGame.prototype.resetGame = function() {
 BowlingGame.prototype.generateFrames = function(pins, rolls) {
   var count;
   for(count = 0; count < rolls; count++) {
-    game.roll(pins)
+    this.roll(pins)
   }
 
 };
